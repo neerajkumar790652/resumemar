33888a6f3c9e2d82efd894ab613e5a81f0937921
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2023-04-25 09:29:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='container'>
+        <h1>Kabir Sagar</h1>
+        <p>Software Developer,Educator at Geekster</p>
+        <a href='#'>Linkedin</a>
+        <a href='#'>Github</a>
+        <hr/>
+        <h2>Experience</h2>
+        <h3>Software Developer At Amazon</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam, unde fuga cumque provident sint velit sapiente modi. Accusamus, eaque obcaecati vitae corporis dolor voluptatibus earum quos rerum, ex expedita, ducimus quo nam quod labore vel blanditiis! Praesentium, amet. Ad accusamus nisi maiores officia voluptate quibusdam, consequatur quaerat excepturi quod modi, quidem dignissimos labore debitis iste in unde nesciunt rem vero provident, voluptatum architecto dolorem velit! Iste dolorem temporibus, incidunt, quod odio eaque voluptas adipisci autem dignissimos reiciendis sunt blanditiis magni, pariatur enim voluptatum laudantium. Omnis, expedita deserunt unde excepturi recusandae, nobis explicabo officiis aliquid impedit provident, ipsa minus. Excepturi, mollitia?</p>
+        <hr/>
+        <h2>Educator at Geekster</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore necessitatibus impedit, quae molestias laudantium, enim officia voluptatum porro architecto sapiente fuga. Quibusdam inventore officia cum ut minima veritatis sint explicabo iure quidem illo rem esse asperiores velit maiores et hic expedita dolore, accusamus quia quae, provident atque! Magni temporibus blanditiis corrupti beatae. Maiores, est consequatur accusamus eveniet aut quos? Veritatis!</p>
+        <hr/>
+        <h2>Skills</h2>
+        <ul>
+          <li>Java</li>
+          <li>Javascript</li>
+          <li>Testing</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>REACTJS</li>
+        </ul>
+        <hr/>
+        <h2>Hobbies</h2>
+        <ol>
+          <li>Table Tennis</li>
+          <li>Reading</li>
+          <li>Biography</li>
+          <li>Teaching</li>
+        </ol>
+      </div>
+    </>
+  )
+}
+
+export default App
